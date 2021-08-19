00679ab1e168a34da8c56c5696fb083621ae665f
Aditi Kandhway <aditikandhway1234@gmail.com>
2021-08-19 23:23:24 +0530

    Added details page

---
+++ Public/js/pages/workshop-details.js
@@ -0,0 +1,7 @@
+// split and extract the string. 
+let search = (window.location.search.substr(1));
+let params=search.split('&');
+const id = params[0].split('=')[1];
+const name=params[1].split('=')[1].replace(/\+/g, ' ');
+console.log(id,name);
+document.querySelector('#workshop-name').innerText=name;
